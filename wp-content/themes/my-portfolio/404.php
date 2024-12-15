58ae3db1225ef3337174457354cbafdd5682c4db
vinicius <vdavidmarques@gmail.com>
2024-12-14 22:25:10 -0300

    feature: Sending 404 page

---
+++ wp-content/themes/my-portfolio/404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+<section class="post not-found">
+   <article>   
+      <h2 class="title">
+         Página não encontrada
+      </h2>
+      <a href="/" target="_self" rel="noopener noreferrer" class="button read-more blue">
+         Início
+      </a>
+   </article>
+</section>
+<?php get_footer(); ?>
